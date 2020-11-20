83f8fa208a38fb70adba5f8004f6f6250dd2699e
Daniel <d.wallgren1987@gmail.com>
2020-11-20 23:03:49 +0100

    Nytt projekt

--- src/timetrack/TimeTrack.java
+++ src/timetrack/TimeTrack.java
@@ -1,21 +1,13 @@
 /*
-
+MAINKLASSEN!!!
  */
 package timetrack;
-//JAG LÄGGER TILL EN KOMMENTAR HÄR!!!!!!!!!!!
-// Test från IntelliJ
+
 import timetrack.model.*;
 import timetrack.gui.*;
 
-/**
- *
- * @author aquar
- */
 public class TimeTrack {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         //Skapar GUI (JFrame objekt) till programmet 
@@ -28,31 +20,5 @@ public class TimeTrack {
         //Gör en dispose för att programmet ska stänga helt efter exit.
         //Programmet kommer endast hit när JFrame stängs
         tGUI.dispose();
-        
-       
-        
-        
-        
-//        User myUser = new User();
-//        myUser.setID(1);
-//        myUser.setName("Kalle");
-//        myUser.setEmail("kalle@example.org");
-//        myUser.setPassword("changeit");
-//        myUser.setAdmin(false);
-        
-        //Print out a oneline string with the user properies using the get methods
-        
-        
-        //System.out.println("ID: " + myUser.getID() + " Name: " + myUser.getName() + " E-mail: " + myUser.getEmail() + " Password: " + myUser.getPassword());
-        //System.out.println(myUser.getAllInfo());
-
-        // try {
-        //   System.out.println("Hello World!" + User.getID + );
-            
-        //} catch (Exception e) {
-        //    System.out.println(e.getMessage());
-        //}
     }
-    
-    
 }
