af7f96e7381358599184777d2d657fe499c43f05
Akram-Mansour <62252350+Akram-Mansour@users.noreply.github.com>
2020-11-24 23:47:39 +0100

    ww

--- src/timetrack/gui/GUIMethod.java
+++ src/timetrack/gui/GUIMethod.java
@@ -5,10 +5,19 @@
  */
 package timetrack.gui;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Akram
  */
 public class GUIMethod {
     
+    public void showDialog() {
+        ImageIcon icon = new ImageIcon("timetrack/timetrack.gui/ic_logo.png");
+        int input = JOptionPane.showConfirmDialog(null,"title", "message", JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE, icon);
+        System.out.println(input);
+            //return input;
+    }
 }
