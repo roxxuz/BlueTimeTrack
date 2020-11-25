031dc3758f226923f2ba948b26f3ceb120a83388
Akram-Mansour <62252350+Akram-Mansour@users.noreply.github.com>
2020-11-25 15:08:44 +0100

    raderat onödiga kommentarer

--- src/timetrack/gui/GUIMethod.java
+++ src/timetrack/gui/GUIMethod.java
@@ -15,8 +15,8 @@ import javax.swing.JOptionPane;
 public class GUIMethod {
     
     public int showDialog(String title, String message) {
-        ImageIcon icon = new ImageIcon("C:\\Users\\Akram\\OneDrive\\Skrivbord\\TimeTrack\\src\\timetrack\\gui\\ic_logo2.png");
-        int input = JOptionPane.showConfirmDialog(null,message,title, JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE, icon);
+        //ImageIcon icon = new ImageIcon("C:\\Users\\Akram\\OneDrive\\Skrivbord\\TimeTrack\\src\\timetrack\\gui\\ic_logo2.png");
+        int input = JOptionPane.showConfirmDialog(null,message,title, JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE);
         
             return input;
            
