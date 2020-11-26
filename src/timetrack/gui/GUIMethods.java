fa1de5d78024715882f1b89153b91705835d5fd6
Daniel <d.wallgren1987@gmail.com>
2020-11-26 14:31:36 +0100

    Bytt namn på GUIMethod till GUIMethods

---
+++ src/timetrack/gui/GUIMethods.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetrack.gui;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Akram
+ */
+public class GUIMethods {
+    
+    public int showDialog(String title, String message) {
+        //ImageIcon icon = new ImageIcon("C:\\Users\\Akram\\OneDrive\\Skrivbord\\TimeTrack\\src\\timetrack\\gui\\ic_logo2.png");
+        int input = JOptionPane.showConfirmDialog(null,message,title, JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE);
+        
+            return input;
+           
+    }
+}
